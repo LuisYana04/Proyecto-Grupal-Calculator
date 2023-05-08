87c253676503aa271056c3489c5a3709cd5b6bcf
Luis <lyanaa@unsa.edu.pe>
2023-05-08 02:48:42 +0000

    Método add() creado

--- Calculator.java
+++ Calculator.java
@@ -1,9 +1,18 @@
-public class Calculator
-{
-    public static void main(String args[]){
-        
-          /** */
-          
-            }
+import java.util.*;
+class Calculator {
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    int entero1=4, entero2=5;
+    
+    System.out.println("La suma de los dos enteros es: "+add(entero1, entero2));
+      
+  }    
+
+public static int add(int num1, int num2){
+      int suma;
+          suma=num1+num2;
+              return suma;
+}
+
 }
 
